31dd46f08375b6447eb99ca07dc1f9458414a1da
Romario Lopez C <romariolopezc@gmail.com>
2016-11-14 18:24:37 -0600

    Exercise 5 STATE done!

--- views/index.jsx
+++ views/index.jsx
@@ -29,9 +29,21 @@ import React from 'react';
   }
 
   class Todo extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {checked: false};
+    }
+
+    handleChange(event) {
+      this.setState({checked: event.target.value})
+    }
+
     render() {
       return (
         <tr>
+          <td style={{border: "1px solid black"}}>
+            <input type="checkbox" checked={this.state.checked} onChange={this.handleChange.bind(this)}/>
+          </td>
           <td style={{border: "1px solid black"}}>{this.props.title}</td>
           <td style={{border: "1px solid black"}}>{this.props.children}</td>
         </tr>
